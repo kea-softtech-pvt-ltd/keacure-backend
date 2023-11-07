e5acfaa26f57f2bec56c813927799882f6fe5324
Roshan Deorukhkar <roshandeorukhkar@gmail.com>
2023-11-07 16:03:14 +0530

    add some admin changes

--- routes/controllers/clinicInfoController.js
+++ routes/controllers/clinicInfoController.js
@@ -1,7 +1,5 @@
 const express        =  require('express');
-const router         =  express.Router();
 const Clinic         =  require('../models/clinicInfo');
-const {clinicInfoSchema} = require('../auth/doctorSchemasValidate')
 module.exports={
     async fetchClinicById (req,res,next){ 
         await Clinic.find({doctorId: req.params.id})
@@ -9,15 +7,14 @@ module.exports={
     },
 
     async insertAllClinic(req,res,next){
-      //  const result = await clinicInfoSchema.validateAsync(req.body)
         const newClinicData    =    new Clinic({
         doctorId         :   req.body.doctorId,
+        clinicLogo       :   req.body.clinicLogo,
         specialization   :   req.body.specialization,
         clinicName       :   req.body.clinicName,
         address          :   req.body.address,
         clinicNumber     :   req.body.clinicNumber,
         services         :   req.body.services,
-       // fees             :   result.fees
         })
         newClinicData.save();
         res.json(newClinicData);
--- routes/models/clinicInfo.js
+++ routes/models/clinicInfo.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const clinicInfo = new mongoose.Schema({
-    clinicId         :mongoose.Schema.Types.ObjectId,
-    doctorId         :mongoose.Schema.Types.ObjectId,
-    specialization   :[],
-    clinicName       :String,
-    address          :String,
-    clinicNumber     :String,
-    services         :[],
+    clinicId         : mongoose.Schema.Types.ObjectId,
+    doctorId         : mongoose.Schema.Types.ObjectId,
+    specialization   : [],
+    clinicLogo       : String,
+    clinicName       : String,
+    address          : String,
+    clinicNumber     : String,
+    services         : [],
 },  {collection: 'clinicInfos' });
 module.exports = Clinic = mongoose.model('clinicInfo', clinicInfo);
